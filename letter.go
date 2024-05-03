fa6c0e56c4bcd43b14b3a7798b9eb6dec68639aa
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2024-05-03 16:33:00 -0400

    generate from a template file

--- letter.go
+++ letter.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-    "time"
-    "strings"
-    "os"
-    "fmt"
-    "log"
-    "math/rand/v2"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strings"
+	"time"
+
+	"math/rand/v2"
 
 	"github.com/johnfercher/maroto/v2/pkg/config"
 	"github.com/johnfercher/maroto/v2/pkg/consts/extension"
@@ -19,7 +21,7 @@ import (
 	"github.com/johnfercher/maroto/v2"
 
 	"github.com/johnfercher/maroto/v2/pkg/components/image"
-	"github.com/johnfercher/maroto/v2/pkg/components/text"
+	mtext "github.com/johnfercher/maroto/v2/pkg/components/text"
 )
 
 type Letter struct {
@@ -40,10 +42,7 @@ func (l *Letter) Generate() {
 
 	today := time.Now().Local().Format("January 02, 2006")
 
-	switch l.Temlate_id {
-	default:
-		l.generalLetterTemplate(today)
-	}
+	l.renderTemplate(today)
 
 	dir_name := fmt.Sprintf("output_%s", strings.ReplaceAll(today, " ", "_"))
 
@@ -51,6 +50,10 @@ func (l *Letter) Generate() {
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if l.document == nil {
+		log.Fatal("letter has no document")
 	}
 
 	err = l.document.Save(
@@ -81,9 +84,52 @@ func (l *Letter) GetMaroto() {
 	l.maroto = maroto.New(cfg)
 }
 
-func (l *Letter) generalLetterTemplate(today string) {
+var valid_fields = []string{
+	"name",
+	"first_names",
+	"company",
+	"street_address",
+	"city_address",
+	"donation_amount",
+	"donation_date",
+}
+
+func (l *Letter) renderTemplate(today string) {
 	if l.maroto == nil {
 		log.Fatal("letter maroto is nil")
+	}
+
+	template := getTemplate("general_ty")
+
+	for _, field := range template.Fields {
+		trim_field := strings.Trim(field, "[]")
+
+		var val string
+
+		switch slices.Contains(valid_fields, trim_field) {
+		case true:
+			switch trim_field {
+			case valid_fields[0]:
+				val = l.Name
+			case valid_fields[1]:
+				val = getFirstNames(l.Name)
+			case valid_fields[2]:
+				val = l.Company
+			case valid_fields[3]:
+				val = l.Street_address
+			case valid_fields[4]:
+				val = l.City_address
+			case valid_fields[5]:
+				val = fmt.Sprintf("%.2f", l.Donation_amount)
+			case valid_fields[6]:
+				val = l.Donation_date
+			}
+
+		case false:
+			val = trim_field
+		}
+
+		template.Content = strings.ReplaceAll(template.Content, field, val)
 	}
 
 	col_width := 12
@@ -96,67 +142,47 @@ func (l *Letter) generalLetterTemplate(today string) {
 		Family: fontfamily.Helvetica,
 	}
 
-	l.maroto.AddRow(40)
-
-	l.maroto.AddRow(5, text.NewCol(col_width, today, text_prop))
-
-	l.maroto.AddRow(vert_gap)
-
-	l.maroto.AddRow(5, text.NewCol(col_width, l.Name, text_prop))
-	if l.Company != "-" {
-		l.maroto.AddRow(5, text.NewCol(col_width, l.Company, text_prop))
-	}
-	l.maroto.AddRow(5, text.NewCol(col_width, l.Street_address, text_prop))
-	l.maroto.AddRow(5, text.NewCol(col_width, l.City_address, text_prop))
-
-	l.maroto.AddRow(vert_gap)
-
-	l.maroto.AddRow(5, text.NewCol(col_width, fmt.Sprintf("Dear %s,", getFirstNames(l.Name)), text_prop))
-
-	l.maroto.AddRow(vert_gap)
-
-	l.maroto.AddRow(14,
-		text.NewCol(
-			col_width,
-			fmt.Sprintf(
-				"On behalf of everyone at Joe’s Movement Emporium, I want to thank you for your generous $%.2f donation received on %s. Your gift ensures top-quality arts education opportunities and experiences continue to thrive here at Joe’s, including:",
-				l.Donation_amount,
-				l.Donation_date),
-			text_prop))
-	l.maroto.AddRow(5, text.NewCol(col_width, "• CreativeWorks", text_prop))
-	l.maroto.AddRow(5, text.NewCol(col_width, "• CreativeGREENWorks", text_prop))
-	l.maroto.AddRow(5, text.NewCol(col_width, "• C.H.O.I.C.E.S.", text_prop))
-	l.maroto.AddRow(5, text.NewCol(col_width, "• Arts Education", text_prop))
-	l.maroto.AddRow(5, text.NewCol(col_width, "• CreateTEEN", text_prop))
-	l.maroto.AddRow(5, text.NewCol(col_width, "• Creative Residencies/Performances", text_prop))
-
-	l.maroto.AddRow(vert_gap)
-
 	text_prop_emphasis := text_prop
 	text_prop_emphasis.Style = fontstyle.BoldItalic
-
-	l.maroto.AddRow(5, text.NewCol(col_width, "Please retain this letter for your records as it confirms that no gifts or services were received in exchange for your donation, which is fully tax-deductible.", text_prop_emphasis))
-
-	l.maroto.AddRow(vert_gap)
-
-	l.maroto.AddRow(5, text.NewCol(col_width, "I look forward to seeing you at Joe’s!", text_prop))
-
-	l.maroto.AddRow(vert_gap)
-
-	l.maroto.AddRow(5, text.NewCol(col_width, "Yours truly,", text_prop))
-	l.maroto.AddRow(vert_gap)
-	l.maroto.AddRow(10, image.NewFromFileCol(10, "./signature.png", props.Rect{Left: text_x, Percent: 100}))
-	l.maroto.AddRow(vert_gap)
-
-	l.maroto.AddRow(5, text.NewCol(col_width, "Brooke Kidd", text_prop))
-	l.maroto.AddRow(5, text.NewCol(col_width, "Executive Director", text_prop))
-
-	l.maroto.AddRow(vert_gap)
 
 	text_prop_small := text_prop
 	text_prop_small.Size = 9
 
-	l.maroto.AddRow(5, text.NewCol(col_width, "World Arts Focus dba Joe’s Movement Emporium is a 501(c)(3) nonprofit organization, donations to which are tax deductible to the fullest extent allowed by law, EIN: 52-180-4860. A copy of our current financial statement is available by writing Joe’s Movement Emporium at 3309 Bunker Hill Road, Mount Rainier, MD 20712 or by calling 301-699-1819.  Documents and information submitted under the Maryland Solicitations Act are also available, for the cost of postage and copies, from the Maryland Secretary of State, State House, Annapolis MD 21401, (410) 974-5534.", text_prop_small))
+	l.maroto.AddRow(40)
+	l.maroto.AddRow(5, mtext.NewCol(col_width, today, text_prop))
+	l.maroto.AddRow(vert_gap)
+
+	for _, block := range strings.Split(template.Content, "\n\n") {
+		leading := 5.0
+		for _, line := range strings.Split(block, "\n") {
+			prop := text_prop
+			if len(line) > 0 {
+				switch line[0] {
+				case '*':
+					prop = text_prop_emphasis
+					leading += 5
+				case '`':
+					line = strings.Trim(line, "`")
+					prop = text_prop_small
+				case '-':
+					if len(line) == 1 {
+						continue
+					}
+				case '.':
+					_, err := os.Stat(line)
+
+					if err == nil {
+						l.maroto.AddRow(10, image.NewFromFileCol(10, line, props.Rect{Left: text_x, Percent: 100}))
+					}
+					continue
+				}
+			}
+
+			l.maroto.AddRow(leading, mtext.NewCol(col_width, line, prop))
+		}
+
+		l.maroto.AddRow(vert_gap)
+	}
 
 	document, err := l.maroto.Generate()
 
---
+++ templates.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Template struct {
+	Setup   string
+	Content string
+	Fields  []string
+}
+
+func getTemplate(file_name string) Template {
+	bytes, _ := os.ReadFile(fmt.Sprintf("./templates/%s.md", file_name))
+
+	sections := strings.Split(string(bytes), "---")
+
+	setup := sections[1]
+	main_content := strings.Trim(sections[2], "\n ")
+
+	//match any word surrounded by square brackets []
+	r, _ := regexp.Compile(`\[.*?\]`)
+
+	fields := r.FindAllString(main_content, -1)
+
+	return Template{
+		Setup:   setup,
+		Content: main_content,
+		Fields:  fields,
+	}
+}
